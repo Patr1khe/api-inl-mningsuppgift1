91c0d17dc341c315e9887a8c784e0fa3a2089eec
Patrik He <patrikswgit@gmail.com>
2023-02-01 00:49:33 +0100

    added import products and router.use for products

--- fed22-api-utveckling-prisma-boilerplate-main/src/routes/index.ts
+++ fed22-api-utveckling-prisma-boilerplate-main/src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import resource from './_router'
+import prodcuts from './products'
 
 // instantiate a new router
 const router = express.Router()
@@ -14,8 +15,15 @@ router.get('/', (req, res) => {
 })
 
 /**
+ * /products
+ */
+router.use('/products', prodcuts)
+
+/**
  * [EXAMPLE] /resource
  */
 // router.use('/resource', resource)
+
+
 
 export default router
