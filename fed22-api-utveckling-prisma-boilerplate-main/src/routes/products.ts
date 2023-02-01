810c62a3107b68a16e8b81116761ae307ceee016
Patrik He <patrikswgit@gmail.com>
2023-02-01 19:13:52 +0100

    created vaildations for router.post for product. Atm fine

---
+++ fed22-api-utveckling-prisma-boilerplate-main/src/routes/products.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { body } from 'express-validator'
+import { index, store } from '../controllers/products_controller'
+const router = express.Router()
+
+router.get('/', index)
+
+router.post('/', [
+    body('name').exists().isString().withMessage('has to be a string').bail().isLength({min: 3, max:100}).withMessage('has to be 3-100 chars long'),
+    body('description').exists().isString().withMessage('has to be a string').bail().isLength({min:20, max: 1000}).withMessage('has to be 20-1000 chars long'),
+    body('price').exists().isInt({min:1}).withMessage('has to be a value').bail().isLength({min:1}).withMessage('has to be atleast 1 value'),
+    // body('images').isJSON().withMessage('has to be a JSON with objects array').bail(),
+    body('stock_status').exists().isString().withMessage('has to be a string').bail(),
+    body('stock_quantity').exists().isInt({min:1}).withMessage('has to be a value').bail(),
+], store)
+
+
+
+
+
+export default router
