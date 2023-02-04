7918faaaca9d610246a4d7a4dced2e895e6b6ef3
Patrik He <patrikswgit@gmail.com>
2023-02-04 04:41:00 +0100

    added validation rules for order and order_items information in postman

--- fed22-api-utveckling-prisma-boilerplate-main/src/routes/orders.ts
+++ fed22-api-utveckling-prisma-boilerplate-main/src/routes/orders.ts
@@ -18,6 +18,22 @@ router.get('/:orderId', show)
  * POST /orders
  */
 router.post('/', [
+    body('customer_first_name').isString().withMessage('has to be a string').bail(),
+    body('customer_last_name').isString().withMessage('has to be a string').bail(),
+    body('customer_address').isString().withMessage('has to be a string').bail(),
+    body('customer_postcode').isString().withMessage('has to be a string').bail().isLength({max:6}).withMessage('postcode max 6 number with string'),
+    body('customer_city').isString().withMessage('has to be a string').bail(),
+    body('customer_email').isEmail().withMessage('has to be a email').bail(),
+    body('customer_phone').optional().custom(async () => {
+        const customer_phone = null
+        if (customer_phone) {
+            return customer_phone
+        }
+    }).withMessage('has to be a string').bail(),
+    body('order_total').isInt({min: 1}).withMessage('has to be a Integer value, higher than 0').bail(),
+    body('product_id').isInt({min: 1}).withMessage('has to be a Integer value, higher than 0').bail(),
+    body('qty').isInt({min: 1}).withMessage('has to be a Integer value, higher than 0').bail(),
+    body('item_total').isInt({min: 1}).withMessage('has to be a Integer value, higher than 0').bail(),
 
 ], store)
 
