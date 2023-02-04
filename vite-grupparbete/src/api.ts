32edb56c638491d7a4c661b009ed28c1b1548300
Patrik He <patrikswgit@gmail.com>
2023-02-04 04:33:48 +0100

    added my localhost:3000/orders and products

--- vite-grupparbete/src/api.ts
+++ vite-grupparbete/src/api.ts
@@ -1,7 +1,7 @@
 import { IOrder, IResponse } from './interfaces'
 
 export const createOrder = async (order : IOrder) => {
-	const res = await fetch("https://bortakvall.se/api/orders", {
+	const res = await fetch("http://localhost:3000/orders", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
@@ -21,7 +21,7 @@ export const createOrder = async (order : IOrder) => {
 }
 
 export const fetchItems = async () => {
-	const res = await fetch("https://bortakvall.se/api/products")
+	const res = await fetch("http://localhost:3000/products")
 
 	if (!res.ok) {
 		throw new Error(`${res.status} ${res.statusText}`)
