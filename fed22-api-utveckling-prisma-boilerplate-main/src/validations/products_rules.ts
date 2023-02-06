643ef702ad5fde4c9a187b33283535fcd702c1cd
Patrik He <patrikswgit@gmail.com>
2023-02-06 14:05:52 +0100

    comment out large and thumbnail, didnt get it to work atm

--- fed22-api-utveckling-prisma-boilerplate-main/src/validations/products_rules.ts
+++ fed22-api-utveckling-prisma-boilerplate-main/src/validations/products_rules.ts
@@ -4,7 +4,7 @@ export const createProductRules = [
     body('name').exists().isString().withMessage('has to be a string').bail().isLength({min: 3, max:100}).withMessage('has to be 3-100 chars long'),
     body('description').exists().isString().withMessage('has to be a string text').bail().isLength({min:20, max: 1000}).withMessage('has to be 20-1000 chars long'),
     body('price').exists().isInt({min:1}).withMessage('has to be a Integer value and need to be higher than 0').bail(),
-    // body('images').isObject().withMessage('has to be an object').bail().notEmpty().withMessage('you need to have image'),
+    body('images').isObject().withMessage('has to be an object').bail(),
     // body('large').isString().withMessage('has to be a string').bail().notEmpty(),
     // body('thumbnail').isString().withMessage('has to be a string').bail().notEmpty(),
     body('stock_status').exists().isString().withMessage('has to be a string').bail(),
