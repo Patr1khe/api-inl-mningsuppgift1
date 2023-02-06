39bcc447592f39942bea2308163674fd5f40c18d
Patrik He <patrikswgit@gmail.com>
2023-02-06 14:09:18 +0100

    imported to correct file for createproductrules validation and removed the old extra validation code

--- fed22-api-utveckling-prisma-boilerplate-main/src/routes/products.ts
+++ fed22-api-utveckling-prisma-boilerplate-main/src/routes/products.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { body } from 'express-validator'
 import { index, store, show, template } from '../controllers/products_controller'
+import { createProductRules } from "../validations/products_rules"
 const router = express.Router()
 
 /**
@@ -17,18 +18,8 @@ router.get('/:productId', show)
  * POST /products
  */
 router.post('/', [
-    body('name').exists().isString().withMessage('has to be a string').bail().isLength({min: 3, max:100}).withMessage('has to be 3-100 chars long'),
-    body('description').exists().isString().withMessage('has to be a string').bail().isLength({min:20, max: 1000}).withMessage('has to be 20-1000 chars long'),
-    body('price').exists().isInt({min:1}).withMessage('has to be a value').bail().isLength({min:1}).withMessage('has to be atleast 1 value'),
-    body('images').isObject().withMessage('has to be a JSON with objects array').bail(),
-    body('stock_status').exists().isString().withMessage('has to be a string').bail(),
-    body('stock_quantity').custom(async () => {
-        const stock_quantity = [Number, null]
-        if (stock_quantity) {
-            return stock_quantity
-        }
-    }).withMessage('has to be a value').bail(),
-], store)
+
+], createProductRules, store)
 
 /**
  * POST /products/template
