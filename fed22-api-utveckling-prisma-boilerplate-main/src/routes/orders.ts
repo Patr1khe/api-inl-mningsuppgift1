3b6dbfe6743e959ff63d7dbb44933bacaec4ef0f
Patrik He <patrikswgit@gmail.com>
2023-02-06 02:12:06 +0100

    partial move to right ts file to import createorderrules

--- fed22-api-utveckling-prisma-boilerplate-main/src/routes/orders.ts
+++ fed22-api-utveckling-prisma-boilerplate-main/src/routes/orders.ts
@@ -1,5 +1,9 @@
 import express from 'express'
+import { body, validationResult } from 'express-validator'
+import { Request, Response } from 'express'
 import { index, store, show } from '../controllers/orders_controller'
+import { createOrderRules } from "../validations/orders_rules"
+import { prisma } from '@prisma/client'
 const router = express.Router()
 
 /**
@@ -16,7 +20,9 @@ router.get('/:orderId', show)
 /**
  * POST /orders
  */
-router.post('/', store)
+router.post('/', [
+
+], createOrderRules, store)
 
 
 
