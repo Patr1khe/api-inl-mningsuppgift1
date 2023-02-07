f20344b51460d77a1a6cf81bda6931d223efcaac
Patrik He <patrikswgit@gmail.com>
2023-02-07 16:19:32 +0100

    added more validation rules for price

--- src/validations/products_rules.ts
+++ src/validations/products_rules.ts
@@ -3,7 +3,7 @@ import {body} from 'express-validator'
 export const createProductRules = [
     body('name').exists().isString().withMessage('has to be a string').bail().isLength({min: 3, max:100}).withMessage('has to be 3-100 chars long'),
     body('description').exists().isString().withMessage('has to be a string text').bail().isLength({min:20, max: 1000}).withMessage('has to be 20-1000 chars long'),
-    body('price').exists().isInt({min:1}).withMessage('has to be a Integer value and need to be higher than 0').bail(),
+    body('price').exists().isInt({min:1}).withMessage('has to be a Integer value and need to be higher than 0').bail().not().isString().withMessage('You cant have string'),
     body('images').isObject().withMessage('has to be an object').bail(),
     body('images.large').isString().withMessage('has to be a string').bail().notEmpty().withMessage('You need to add image for large'),
     body('images.thumbnail').isString().withMessage('has to be a string').bail().notEmpty().withMessage('You need to add image for thumbnail'),
